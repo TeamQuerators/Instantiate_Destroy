1d7be915496ef0c15fe82389b7900e1f2e300d93
Team Querators <teamquerators@gmail.com>
2019-04-08 19:11:57 +0800

    Third commit
    - Test different drag objects

---
+++ Instantiate_Destroy/Assets/Scripts/GameobjectDragAndDrop.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameobjectDragAndDrop : MonoBehaviour
+{
+    public string mousecontrolled = "n";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+
+    void OnMouseDrag()
+    {
+
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos;
+    }
+
+    
+    /*void Update()
+    {
+        if (mousecontrolled == "y")
+        {
+            //mousecontrolled = "y"; //subject to change
+            Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = objPosition;
+        }
+
+    }
+
+    void OnMouseDown()
+    {
+        mousecontrolled = "y";
+        //gameplay.selectedSandwich = occupiedSlot;
+
+    }*/
+}
---
+++ Instantiate_Destroy/Assets/Scripts/InputManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputManager : MonoBehaviour
+{
+    private bool draggingItem = false;
+    private GameObject draggedObject;
+    private Vector2 touchOffset;
+    
+	void Update ()
+	{
+	    if (HasInput)
+	    {
+	        DragOrPickUp();
+	    }
+	    else
+	    {
+	        if (draggingItem)
+	            DropItem();
+	    }
+	}
+    Vector2 CurrentTouchPosition
+    {
+        get
+        {
+           return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+    private void DragOrPickUp()
+    {
+        var inputPosition = CurrentTouchPosition;
+        if (draggingItem)
+        {
+            draggedObject.transform.position = inputPosition + touchOffset;
+        }
+        else
+        {
+            RaycastHit2D[] touches = Physics2D.RaycastAll(inputPosition, inputPosition, 0.5f);
+            if (touches.Length > 0)
+            {
+                var hit = touches[0];
+                if (hit.transform != null)
+                {
+                    draggingItem = true;
+                    draggedObject = hit.transform.gameObject;
+                    touchOffset = (Vector2)hit.transform.position - inputPosition;
+                    draggedObject.transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+                }
+            }
+        }
+    }
+    private bool HasInput
+    {
+        get
+        {
+            // returns true if either the mouse button is down or at least one touch is felt on the screen
+            return Input.GetMouseButton(0);
+        }
+    }
+    void DropItem()
+    {
+        draggingItem = false;
+        draggedObject.transform.localScale = new Vector3(1, 1, 1);
+    }
+}
